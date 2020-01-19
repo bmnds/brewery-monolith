a6bc5300adf855f8ea70ee79dc7adfaee29cf14f
Bruno Mendes <b.mendes00@gmail.com>
2020-01-18 21:19:55 -0400

    Implementing validation for Beer Models

--- src/main/java/guru/sfg/brewery/web/model/BeerDto.java
+++ src/main/java/guru/sfg/brewery/web/model/BeerDto.java
@@ -17,38 +17,46 @@
 
 package guru.sfg.brewery.web.model;
 
+import java.math.BigDecimal;
+import java.time.OffsetDateTime;
+import java.util.UUID;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.PositiveOrZero;
+
 import com.fasterxml.jackson.annotation.JsonFormat;
+
 import lombok.Builder;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
-
-import java.math.BigDecimal;
-import java.time.OffsetDateTime;
-import java.util.UUID;
 
 @Data
 @NoArgsConstructor
 @EqualsAndHashCode(callSuper = true)
 public class BeerDto extends BaseItem {
 
-    @Builder
-    public BeerDto(UUID id, Integer version, OffsetDateTime createdDate, OffsetDateTime lastModifiedDate, String beerName,
-                   BeerStyleEnum beerStyle, String upc, Integer quantityOnHand, BigDecimal price) {
-        super(id, version, createdDate, lastModifiedDate);
-        this.beerName = beerName;
-        this.beerStyle = beerStyle;
-        this.upc = upc;
-        this.quantityOnHand = quantityOnHand;
-        this.price = price;
-    }
+	@Builder
+	public BeerDto(UUID id, Integer version, OffsetDateTime createdDate,
+			OffsetDateTime lastModifiedDate, String beerName,
+			BeerStyleEnum beerStyle, String upc, Integer quantityOnHand,
+			BigDecimal price) {
+		super(id, version, createdDate, lastModifiedDate);
+		this.beerName = beerName;
+		this.beerStyle = beerStyle;
+		this.upc = upc;
+		this.quantityOnHand = quantityOnHand;
+		this.price = price;
+	}
 
-    private String beerName;
-    private BeerStyleEnum beerStyle;
-    private String upc;
-    private Integer quantityOnHand;
+	@NotNull
+	private String beerName;
+	private BeerStyleEnum beerStyle;
+	private String upc;
+	@PositiveOrZero
+	private Integer quantityOnHand;
 
-    @JsonFormat(shape= JsonFormat.Shape.STRING)
-    private BigDecimal price;
+	@JsonFormat(shape = JsonFormat.Shape.STRING)
+	private BigDecimal price;
 
 }
